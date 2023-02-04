ade725498768a5550c693da1c12973c1a9e3ed3c
kravets-julia <kravets_julia@i.ua>
2023-02-04 16:43:28 +0200

    02-Vimeo_player

--- src/index.js
+++
@@ -1,2 +0,0 @@
-import SimpleLightbox from "simplelightbox";
-import img from './js/01-gallery'
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// import { throttle } from 'throttle-debounce';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+
+    const KEY = 'videoplayer-current-time';
+    
+    videoStorage()
+
+
+    player.on('timeupdate', function (time) {
+       
+        throttle (localStorage.setItem(KEY, `${time.seconds}`), 1000)
+        console.log(time)});
+
+function videoStorage(){
+    const storage = localStorage.getItem(KEY)
+    if (storage) 
+    {player.setCurrentTime(localStorage.getItem(KEY))}
+    console.log(storage)
+}
+
+   

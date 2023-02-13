9f719b41cd40d19b647e6ba14b1d25f19fdfedca
kravets-julia <kravets_julia@i.ua>
2023-02-13 11:14:20 +0200

    object

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,16 +5,11 @@ const STORAGE_KEY = 'feedback-form-state'
 const formEl =  document.querySelector('.feedback-form');
 const inputEl = document.querySelector('.feedback-form input')
 const messageEl = document.querySelector('.feedback-form textarea')
-console.log(formEl)
-console.log(inputEl)
-console.log(messageEl)
 
 populateText ()
 
 formEl.addEventListener('input', throttle(onTextInput, 500))
 formEl.addEventListener('submit', onFormSubmit)
-// inputEl.addEventListener('input', onTextInput)
-// messageEl.addEventListener('input', onTextMessage)
 
 function onFormSubmit (e){
     e.preventDefault();
@@ -22,17 +17,16 @@ function onFormSubmit (e){
         email: `${inputEl.value}`,
         message: `${messageEl.value}`
         }
-        const {elements: { email, message } } = e.currentTarget
+        const {elements: { email, message } } = e.currentTarget.value
     if 
         (  email.value === '' || message.value === '') {
             return alert ('Please fill all the fields!')
         }
-
+        
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY)
     console.log(formElemets)
    }
-
 
 function onTextInput(e){
   localStorage.setItem(STORAGE_KEY, JSON.stringify({email: `${inputEl.value}`, message: `${messageEl.value}`}))
@@ -40,12 +34,10 @@ function onTextInput(e){
 
 function populateText (){
     const savedData = localStorage.getItem(STORAGE_KEY);
-
     const parsedData = JSON.parse(savedData);
-console.log(parsedData)
     if(parsedData){
-        inputEl.value = parsedData.email;
-        messageEl.value = parsedData.message
+        inputEl.value = parsedData?.email||'';
+        messageEl.value = parsedData?.message||''
     }
 }
 
